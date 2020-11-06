c2d4a97a341d3a93fb69330a739f55197ed27290
Austin Klein <austinklein34@gmail.com>
2020-11-06 09:38:10 -0700

    adding animations and bug fixing

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const list = document.querySelector('.list');
+let five = document.querySelector('.five');
 
 /* delete item */
 
 list.addEventListener('click', function(e) {
     if (e.target.className === 'delete') {
         const li = e.target.parentElement;
-        list.removeChild(li);
+        li.classList.add('line');
+        setTimeout(function() { list.removeChild(li) }, 400);
     }
 });
 
@@ -26,7 +28,7 @@ addBtn.addEventListener('click', function addItem() {
     const value = input.value;
 
     if (listOfItems.length < 5 && value !== '') {
-
+        five.style.display = 'none';
         const item = document.createElement('li');
         const text = document.createElement('div');
         const deleteBtn = document.createElement('span');
@@ -37,7 +39,6 @@ addBtn.addEventListener('click', function addItem() {
         item.classList.add('item');
         text.classList.add('text');
         deleteBtn.classList.add('delete');
-
         item.appendChild(text)
         item.appendChild(deleteBtn)
         list.appendChild(item);
@@ -46,7 +47,8 @@ addBtn.addEventListener('click', function addItem() {
     } else if (listOfItems.length < 5 && value === '') {
 
     } else if (listOfItems.length >= 5 && value !== '') {
-        alert('You already have 5 tasks, please complete these first!');
+        five.style.display = 'inherit';
+        setTimeout(function() { five.style.display = 'none' }, 3000);
     }
 
     input.value = '';
